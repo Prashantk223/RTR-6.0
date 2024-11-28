a304e5301978fcb744fb4861394ac5be5c138303
Prashant <prashant.kamble223@gmail.com>
2024-11-29 00:13:21 +0530

    C 5th Assignment

---
+++ 05-EscapeSequences/EscapeSequences.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+	printf("\n\n");
+	printf(" GOing to next line....using\\n Escape Sequence\n\n");
+	printf(" Demonstrating \t Horizontal \t Tab \t Using \t \\t Escape Sequence !!!\n\n");
+	printf("\ This is a Double Quoted Output\ Done Using \\\" \\\" Escape Sequence\n\n");
+	printf("\ This is a Double Quoted Output\ Done Usinhg \\\' \\\' Escape Sequence\n\n");
+	printf("BACKSPACE Turned TO BACKSPACE\b Using Escape Swquence \\b\n\n");
+	printf("\r Demonstrating Carriage Return Using \\r Escape Sequence\n\n");
+	printf("Demonstrating \r Carriage Return Using \\r Escape Sequence\n\n");
+	printf("Demonstrating Carriage Return \r Using \\r Escape Sequence\n\n");
+	printf("Demonstrating \x41 Using \\xhh Escape Sequence\n\n");
+	printf("Demonstrating \102 Using \\ooo Escape Sequence\n\n");
+		
+	return 0;
+}
